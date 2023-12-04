dd27d33c46095c511c66baae3dc3e11d81faed26
felixzuo <felix.zuo@cheilpengtai.com>
2023-12-04 17:03:28 +0800

    lie

--- abc.java
+++ abc.java
@@ -1,30 +1,7 @@
-String priceDisplayYn =  commonDataService.getcommonB2BData(siteCode, "isPricedisplay", "N", true);
-			String priceCurrency =  commonDataService.getcommonB2BData(siteCode, "priceCurrency", "", true);
-			String calculateFinancingYn =  commonDataService.getcommonB2BData(siteCode, "calculateFinancingYn", "N", true);
-			
-			String emiUrl = "";
-			String financingUrl = "";
-			String smbRegistrationUrl = "";
-			if ( runModes.contains("prod")  && runModes.contains("live") && !runModes.contains("soft")) {
-				emiUrl = commonDataService.getcommonB2BData(siteCode, "prodEmiUrl", "", true);
-				financingUrl = commonDataService.getcommonB2BData(siteCode, "prodFinancingUrl", "", true);
-				smbRegistrationUrl = commonDataService.getcommonB2BData(siteCode, "smbRegistrationUrl", "", true);
-				if(runModes.contains("ro") || runModes.contains("preqa") || runModes.contains("preqa2")){
-					smbRegistrationUrl = commonDataService.getcommonB2BData(siteCode, "stgSmbRegistrationUrl", "", true);
-				}
-	        } else {
-	        	emiUrl = commonDataService.getcommonB2BData(siteCode, "stgEmiUrl", "", true);
-	        	financingUrl = commonDataService.getcommonB2BData(siteCode, "stgFinancingUrl", "", true);
-				smbRegistrationUrl = commonDataService.getcommonB2BData(siteCode, "stgSmbRegistrationUrl", "", true);
-	        }
-			
-			String hybrisApiJson =  commonDataService.getcommonB2BData(siteCode, "hybrisApiJson", "N", true);
-			
-			OPLMetaSlingModel metaModel = new OPLMetaSlingModel();
-			metaModel.setCalculateFinancingYn(calculateFinancingYn);
-			metaModel.setCartUrl(cartUrl);
-			metaModel.setEmiUrl(emiUrl);
 
-    3 matser off line
+
+
 1. modified by oliver.zuo in master  branch on line
 2 mod:over.zuo dev branch off line.
+3 matser off line
+4.stg offline
